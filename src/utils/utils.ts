f2e3e268b132d4927643c67bd8959b1a62b05c20
mediocre9 <mirzafahadzia9@gmail.com>
2025-12-14 16:12:29 +0500

    fix: `flattenVersion` deprecated and enhanced by `getNormalizedVersion` into padded digits

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -4,19 +4,58 @@ import * as crypto from "node:crypto";
 import * as semver from "semver";
 import { uniqueNamesGenerator, adjectives, colors } from "unique-names-generator";
 import { spaceNames } from "./space-dictionary-names";
+import { log } from "node:console";
+
+export type Nullable<T> = T | null;
+export type NullableOrUndefined<T> = T | null | undefined;
 
 /**
  *
- * @todo **FADI MOVE THIS TO SOMEWHERE ELSE.. DOES NOT SEEMS TO BELONG HERE....**
+ * @todo **FADI MOVE THIS TO SOMEWHERE ELSE, NOT IN UTILS......**
  */
 export function isDevEnvironment(): boolean {
   return process.env.NODE_ENV === "development";
 }
 
+/**
+ * @deprecated This function has been deprecated.
+ * Use `getNormalizedVersion` function.
+ * @param version
+ * @returns version in numbers
+ */
 export function flattenVersion(version: string): number {
   const versionWithoutPeriods: string = version.replaceAll(".", "");
   const versionInNumber: number = parseInt(versionWithoutPeriods);
   return versionInNumber;
+}
+
+/**
+ *
+ * @param version
+ * @description Converts semver version into a normalized padded 7-digit `0s` numeric value.
+ * @example "1.1.19" => 100000010000019
+ * @returns normalized number version
+ */
+export function getNormalizedVersion(version: string): number {
+  const PADDING_SIZE = 7;
+  const chunks: string[] = version.split(".");
+
+  if (chunks[0].split("").length > PADDING_SIZE) {
+    throw new Error("MAJOR version length is greater than 7!");
+  }
+
+  if (chunks[1].split("").length > PADDING_SIZE) {
+    throw new Error("MINOR version length is greater than 7!");
+  }
+
+  if (chunks[2].split("").length > PADDING_SIZE) {
+    throw new Error("PATCH version length is greater than 7!");
+  }
+
+  chunks[1] = chunks[1].padStart(PADDING_SIZE, "0");
+  chunks[2] = chunks[2].padStart(PADDING_SIZE, "0");
+  const normalized = chunks.join("");
+  return parseInt(normalized);
 }
 
 export function generateOTPCode(length: number = 6): string {
@@ -30,7 +69,7 @@ export function generateOTPCode(length: number = 6): string {
   return code;
 }
 
-export function getUniqueGeneratedUserName(): string {
+export function generateRandomSpaceUsername(): string {
   return uniqueNamesGenerator({
     dictionaries: [adjectives, colors, spaceNames],
     style: "lowerCase",
@@ -38,12 +77,26 @@ export function getUniqueGeneratedUserName(): string {
 }
 
 export function isSemver(version: string): boolean {
-  return semver.valid(version) !== null ? true : false;
+  try {
+    const isCleanFormat = semver.clean(version, { loose: false });
+    if (!isCleanFormat) return false;
+
+    const coerced = semver.coerce(version, { loose: false, includePrerelease: false });
+    if (!coerced) return false;
+
+    const isValid = semver.valid(coerced);
+    if (!isValid) return false;
+
+    return true;
+  } catch (error) {
+    log(error);
+    throw error;
+  }
 }
 
 /**
  *
- * @description Generates a random API key with an abbreviated prefix of voyager.
+ * @description Generates a random API key with an abbreviated prefix of abbreaviation of voyager as vygr_.
  * @example vygr_RANDOM_API_KEY_VALUE
  * @returns strings
  */
@@ -51,7 +104,7 @@ export function generateApiKey(): string {
   return "vygr_" + crypto.randomBytes(16).toString("hex");
 }
 
-export async function generateFileHashSignature(filename: string): Promise<string> {
+export async function generateFileHash(filename: string): Promise<string> {
   return new Promise((resolve, reject) => {
     const fileStream = fs.createReadStream(filename);
 
