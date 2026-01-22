bf0c8d9871f03425d37f987caab59dbedfc38152
mediocre9 <mirzafahadzia9@gmail.com>
2026-01-22 23:56:42 +0500

    refactor: rename `measurePerfomanceAsync` to `computeElapsedTimeAsync` and `measurePerfomanceSync` to `computeElapsedTimeSync`

--- src/utils/performance.ts
+++ src/utils/performance.ts
@@ -1,14 +1,14 @@
 type AsyncFn = () => Promise<void>;
 type SyncFn = () => void;
 
-export async function measurePerfomanceAsync(fn: AsyncFn): Promise<number> {
+export async function computeElapsedTimeAsync(fn: AsyncFn): Promise<number> {
   const startTime = performance.now();
   await fn();
   const elapsedTime = performance.now() - startTime;
   return elapsedTime;
 }
 
-export function measurePerfomanceSync(fn: SyncFn): number {
+export function computeElapsedTimeSync(fn: SyncFn): number {
   const startTime = performance.now();
   fn();
   const elapsedTime = performance.now() - startTime;
