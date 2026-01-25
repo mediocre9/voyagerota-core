bf33385263456f8f9d49f136da9929130e08c57b
Mediocre <76650975+mediocre9@users.noreply.github.com>
2026-01-25 19:19:00 +0500

    Revert "refactor: change `CRON` time scheduling based on prod and dev environments - Prod Environment Cron time is weekend midnights - Dev Environment Cron time is 3 mins"
    This reverts commit f2f4495a0e096beda295420fd191ccd6ff4901c9.

--- src/cron/purge.cron.ts
+++ src/cron/purge.cron.ts
@@ -1,37 +1,41 @@
 import { db } from "@config/db.config";
+import * as ProjectDAL from "@dal/project.dal";
 import * as ReleaseDAL from "@dal/release.dal";
-import * as ArtifactDAL from "@dal/artifact.dal";
+import * as TelemetryDAL from "@dal/telemetry.dal";
 import { Logger } from "@utils/logger";
-import { isDevEnvironment } from "@config/config";
+import { isDevEnvironment } from "@utils/utils";
 import { CronJob } from "cron";
-import { FileStorageOperation } from "@utils/common";
 
-// *Runs on weekends 2 times a day at 12:00am......
-// *for development 3 minutes.....
-const CRON_EXPRESSION = isDevEnvironment() ? "*/180 * * * * *" : "0 0 0 * * 6,0";
+// *Runs on weekends 2 times a day at 0:00 and 12:00......
+const CRON_EXPRESSION = isDevEnvironment() ? "*/20 * * * * *" : "0 0 0,12 * * 6,0";
 
 // Removes records completely that are 30 days old....
-export const PurgingCronJob = new CronJob(CRON_EXPRESSION, async () => {
+export const PurgeCronJob = new CronJob(CRON_EXPRESSION, async () => {
   Logger.info("Cron Job Fired!");
 
   const MAX_DAYS = 30;
   const MAX_DAYS_IN_MILLISECONDS = isDevEnvironment()
-    ? new Date(Date.now() - 10 * 60 * 1000) // 10 minutes older records.....
+    ? new Date(Date.now() - 2 * 60 * 1000) // 2 min past.....
     : new Date(Date.now() - MAX_DAYS * 60 * 60 * 24 * 1000);
 
   const transaction = await db.transaction();
   try {
     const releases = await ReleaseDAL.getSoftDeletedReleases(MAX_DAYS_IN_MILLISECONDS);
-    const artifacts = await ArtifactDAL.getSoftDeletedArtifacts(MAX_DAYS_IN_MILLISECONDS);
-
     if (releases.length > 0) {
-      for (const artifact of artifacts) {
-        await FileStorageOperation.removePermanently(artifact.filename!);
-      }
-
-      await ArtifactDAL.purgeAllArtifacts(MAX_DAYS_IN_MILLISECONDS, transaction);
       await ReleaseDAL.purgeAllReleases(MAX_DAYS_IN_MILLISECONDS, transaction);
       Logger.info("Releases have been purged!");
+    }
+
+    const telemetries = await TelemetryDAL.getSoftDeletedTelemetry(MAX_DAYS_IN_MILLISECONDS);
+    if (telemetries.length > 0) {
+      await TelemetryDAL.purgeAllTelemetry(MAX_DAYS_IN_MILLISECONDS, transaction);
+      Logger.info("Telemtry has been purged!");
+    }
+
+    const projects = await ProjectDAL.getSoftDeletedProjects(MAX_DAYS_IN_MILLISECONDS);
+    if (projects.length > 0) {
+      await ProjectDAL.purgeAllProjects(MAX_DAYS_IN_MILLISECONDS, transaction);
+      Logger.info("Projects have been purged!");
     }
     await transaction.commit();
 
