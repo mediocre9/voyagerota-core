6d8de0df5aa4e360f11a25d3ea8a534fe34ae60c
mediocre9 <mirzafahadzia9@gmail.com>
2026-01-25 17:32:39 +0500

    refactor: improve `ArtifactInspectionTaskService` with improve task queue logic

--- src/services/artifact.task.service.ts
+++ src/services/artifact.task.service.ts
@@ -4,17 +4,22 @@ import { ArtifactTaskDTO } from "@schemas/artifact.schema";
 import { ApiError } from "@utils/error";
 import { StatusCodes } from "http-status-codes";
 import { inject, injectable } from "tsyringe";
-import { ArtifactQueueService, TaskCreatedData, TaskStatus } from "./artifact.queue.service";
+import {
+  ArtifactInspectionQueueService,
+  TaskResultData,
+  TaskStatus,
+} from "./artifact.queue.service";
+import { TaskIdPathParam } from "@schemas/task.schema";
+import { Logger } from "@utils/logger";
 
-// TODO Add processed | pending states in artifact table to enforce release promotion only to staging once ...
 @injectable()
-export class ArtifactTaskService {
+export class ArtifactInspectionTaskService {
   constructor(
-    @inject(ArtifactQueueService)
-    private readonly _queue: ArtifactQueueService
+    @inject(ArtifactInspectionQueueService)
+    private readonly _queue: ArtifactInspectionQueueService,
   ) {}
 
-  public async createTask(dto: ArtifactTaskDTO): Promise<TaskCreatedData> {
+  public async createTask(dto: ArtifactTaskDTO): Promise<TaskResultData> {
     const { releaseId, filename } = dto;
 
     const release = await ReleaseDAL.findReleaseByPublicId(releaseId);
@@ -22,34 +27,42 @@ export class ArtifactTaskService {
       throw new ApiError("releaseId Not Found!", StatusCodes.NOT_FOUND);
     }
 
-    // !remove the explicit error but put it in documentation......
-    const artifact = await ArtifactDAL.findArtifactByReleaseId(release.id!);
+    const artifact = await ArtifactDAL.findPendingArtifact(release.getId(), filename);
     if (!artifact) {
-      throw new ApiError(
-        "Draft releases cannot be processed! Please upload file to promote release as staging!",
-        StatusCodes.BAD_REQUEST
-      );
+      throw new ApiError("Artifact not found!", StatusCodes.NOT_FOUND);
     }
 
-    const job = await this._queue.putJob({
-      releaseId: releaseId,
-      filename: filename,
-      hash: artifact.hash!,
+    if (artifact.isProcessed()) {
+      throw new ApiError("This artifact has been already processed!", StatusCodes.CONFLICT);
+    }
+
+    const taskResult = await this._queue.putJob({
+      artifactId: artifact.getPublicId(),
+      releaseId: release.getPublicId(),
+      releaseInternalId: release.getId(),
+      filename: artifact.getFileName(),
     });
 
-    return job;
+    return taskResult;
   }
 
-  public async getTaskStatus(jobId: string): Promise<TaskStatus> {
-    const job = await this._queue.getJob(jobId);
+  public async getTaskStatus(pathParam: TaskIdPathParam): Promise<TaskStatus> {
+    const { taskId } = pathParam;
+    const job = await this._queue.getJob(taskId);
 
     if (!job) {
       throw new ApiError("Task not found!", StatusCodes.NOT_FOUND);
     }
 
-    const status = await this._queue.getCurrentJobStatus(jobId);
-    const state = this._queue.getJobState(status);
-    const jobData = await this._queue.getData(jobId);
-    return { state: state, status: status, data: jobData };
+    const state = await this._queue.getCurrentJobState(taskId);
+    const status = this._queue.getJobStatus(state);
+    const jobData = await this._queue.getData(taskId);
+    const taskStatus: TaskStatus = {
+      status: status,
+      state: state,
+      data: jobData,
+    };
+
+    return taskStatus;
   }
 }
