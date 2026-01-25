4f4f5fa3e54b5cb91b88ae4badf55d82ea6bccce
Mediocre <76650975+mediocre9@users.noreply.github.com>
2026-01-25 19:19:00 +0500

    Revert "refactor: imrpove `ArtifactStorageService` with improved logic with better redundant file cleanup operations"
    This reverts commit 8e0532da2449e1dc0f71f383e591b88bf4c616e3.

--- src/services/artifact.storage.service.ts
+++ src/services/artifact.storage.service.ts
@@ -2,24 +2,22 @@ import { db } from "@config/db.config";
 import * as ArtifactDAL from "@dal/artifact.dal";
 import * as ProjectDAL from "@dal/project.dal";
 import * as ReleaseDAL from "@dal/release.dal";
-import { ArtifactDTO } from "@models/release.model";
 import { ReleaseArtifactIdPathParams, ReleaseIdPathParam } from "@schemas/release.schema";
-import { FileStorageOperation } from "@utils/common";
 import { ApiError } from "@utils/error";
 import { Logger } from "@utils/logger";
 import * as Utils from "@utils/utils";
 import * as crypto from "crypto";
 import { StatusCodes } from "http-status-codes";
 import multer from "multer";
+import * as fs from "node:fs/promises";
 import * as path from "node:path";
-import { inject, injectable } from "tsyringe";
-import { ReleaseCacheService } from "./cache.service";
+import { injectable } from "tsyringe";
 
 const storage = multer.diskStorage({
-  destination: function (_, __, callback) {
-    callback(null, FileStorageOperation.DEFAULT_FILE_STORAGE_PATH);
+  destination: function (request, file, callback) {
+    callback(null, path.join(import.meta.dirname, "..", "..", "storage"));
   },
-  filename: function (_, file, callback) {
+  filename: function (request, file, callback) {
     const id = crypto.randomUUID();
     const originalFilename = file.originalname;
     const uniqueFilename = `$${id}-${originalFilename}`;
@@ -30,17 +28,9 @@ const storage = multer.diskStorage({
 export const MAX_FILE_SIZE_IN_BYTES = 8e6; // 8mb in bytes....
 export const upload = multer({ storage: storage });
 
-/**
- * TODO: [PRI-02] Remove the FileStorageOperation operations from this service....
- */
 // ** [DONE] implement file dedup checks.......
 @injectable()
 export class ArtifactStorageService {
-  constructor(
-    @inject(ReleaseCacheService)
-    private readonly _cache: ReleaseCacheService,
-  ) {}
-
   public async deleteBlob(payload: ReleaseArtifactIdPathParams) {
     const { releaseId, artifactId } = payload;
     const release = await ReleaseDAL.findReleaseByPublicId(releaseId);
@@ -51,55 +41,22 @@ export class ArtifactStorageService {
     const artifact = await ArtifactDAL.findArtifactById(artifactId);
 
     if (!artifact) {
-      throw new ApiError("File not found!", StatusCodes.NOT_FOUND);
+      throw new ApiError("File blob not found!", StatusCodes.NOT_FOUND);
     }
 
-    if (release.isRevoked()) {
-      throw new ApiError("Revoked release artifact cannot be deleted!", StatusCodes.CONFLICT);
-    }
+    const fileStoragePath = path.join(
+      import.meta.dirname,
+      "..",
+      "..",
+      "storage",
+      artifact.filename!
+    );
 
-    if (release.isProduction()) {
-      throw new ApiError("Production release artifact cannot be deleted!", StatusCodes.CONFLICT);
-    }
-
-    const transaction = await db.transaction();
-    try {
-      await ArtifactDAL.deleteByFilename(artifact.getFileName(), transaction);
-      await ReleaseDAL.updateReleaseChannel(releaseId, "draft", transaction);
-      await FileStorageOperation.removePermanently(artifact.getFileName(), "storage");
-
-      const project = await ProjectDAL.findProjectByInternalId(release.getProjectForeignKeyId());
-
-      // * invalidate staging release if exist in cache store....
-      // * as release falls back to draft state on artifact deletion....
-      const { isInvalidated, cacheKey } = await this._cache.invalidateCache(
-        project!.getPublicId(),
-        "staging",
-      );
-      if (isInvalidated) {
-        Logger.info(`Invalidated artifact ${cacheKey} from cache!`);
-      }
-      await transaction.commit();
-      Logger.info("Artifact deleted!");
-    } catch (error) {
-      Logger.error("Artifact Deletion Error ", error as string);
-      await transaction.rollback();
-    }
-  }
-
-  public async findUploadedArtifacts(releaseId: string) {
-    const release = await ReleaseDAL.findReleaseByPublicId(releaseId);
-    if (!release) {
-      throw new ApiError("Release not found!", StatusCodes.BAD_REQUEST);
-    }
-
-    const artifact = await ArtifactDAL.findArtifactByInternalId(release.getId());
-
-    if (!artifact) {
-      throw new ApiError("artifact not found!", StatusCodes.NOT_FOUND);
-    }
-
-    return artifact;
+    // !use transactions...
+    await ArtifactDAL.deleteByFilename(artifact.filename!);
+    await ReleaseDAL.updateStatus(releaseId, "draft");
+    await fs.rm(fileStoragePath);
+    Logger.info("File deleted!");
   }
 
   public async getBinaryFilePath(params: ReleaseArtifactIdPathParams) {
@@ -115,12 +72,15 @@ export class ArtifactStorageService {
       throw new ApiError("artifact not found!", StatusCodes.NOT_FOUND);
     }
 
-    const pathToFile = path.join(
-      FileStorageOperation.DEFAULT_FILE_STORAGE_PATH,
-      artifact.getFileName(),
+    const fileStoragePath = path.join(
+      import.meta.dirname,
+      "..",
+      "..",
+      "storage",
+      artifact.filename!
     );
 
-    return pathToFile;
+    return fileStoragePath;
   }
 
   public async save(
@@ -128,54 +88,57 @@ export class ArtifactStorageService {
       Express.Multer.File,
       "filename" | "originalname" | "mimetype" | "size" | "fieldname"
     >,
-    param: ReleaseIdPathParam,
-  ): Promise<ArtifactDTO> {
+    param: ReleaseIdPathParam
+  ) {
     const { releaseId } = param;
     const release = await ReleaseDAL.findReleaseByPublicId(releaseId);
     if (!release) {
       throw new ApiError("Release not found!", StatusCodes.BAD_REQUEST);
     }
 
-    /**
-     * * If new file is uploaded and has no conflict with prior release artifacts then it gets saved to db....
-     * * If again same file is uploaded then "Same artifact already exists!" error is thrown...
-     * * If new file is uploaded after first successful save then "Another artifact already exists!" error is thrown..
-     */
     const hash = await Utils.generateFileHash(path.join("storage", file.filename));
 
-    if (await ArtifactDAL.isHashFound(release.getId(), hash)) {
-      const artifact = await ArtifactDAL.findPendingArtifact(release.getId(), file.originalname);
-      await artifact?.destroy({ force: true });
-      await this.deleteBinaryFromStorage(artifact!.filename!);
-    }
+    const collision = await ProjectDAL.isHashFoundAcrossReleases(release.project_id_fk!, hash);
 
-    const collisionRelease = await ProjectDAL.isHashFoundAcrossReleases(
-      release.getProjectForeignKeyId(),
-      hash,
-    );
-
-    if (collisionRelease) {
+    if (collision) {
       await this.deleteBinaryFromStorage(file.filename);
       throw new ApiError(
-        `Collision detected with release v${collisionRelease.Releases![0].version}!`,
-        StatusCodes.CONFLICT,
+        `Collision detected with release v${collision.Releases![0].version} with artifact id ${
+          collision.Releases![0].ArtifactFiles![0].public_id
+        }`,
+        StatusCodes.CONFLICT
       );
     }
 
-    const artifact = await ArtifactDAL.createOrUpdate(release.getId(), {
-      filename: file.filename,
-      size: file.size,
-      hash: hash,
-      originalFileName: file.originalname,
-    });
+    if (await ArtifactDAL.isHashFound(release.id!, hash)) {
+      await this.deleteBinaryFromStorage(file.filename);
+      throw new ApiError("Same file already exists!", StatusCodes.CONFLICT);
+    }
 
-    return artifact.toDTO();
+    const transaction = await db.transaction();
+    try {
+      const artifact = await ArtifactDAL.createOrUpdate(release.id!, {
+        filename: file.filename,
+        size: file.size,
+        hash: hash,
+      });
+
+      await ReleaseDAL.updateStatus(release.public_id!, "staging");
+      await transaction.commit();
+      return artifact.toDTO();
+    } catch (error) {
+      await transaction.rollback();
+      await this.deleteBinaryFromStorage(file.filename);
+      throw error;
+    }
   }
 
   public async deleteBinaryFromStorage(filename: string): Promise<void> {
+    const fileStoragePath = path.join(import.meta.dirname, "..", "..", "storage", filename);
+
     try {
-      await FileStorageOperation.removePermanently(filename, "storage");
-      Logger.info("file deleted! ".concat(filename));
+      await fs.rm(fileStoragePath);
+      Logger.warn("file deleted! ".concat(filename));
     } catch (error) {
       Logger.error((error as Error).message);
       throw error;
