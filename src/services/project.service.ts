385c3cbf4a38eecdb7d9effe082dc386b0d7df11
mediocre9 <mirzafahadzia9@gmail.com>
2026-01-25 17:54:16 +0500

    refactor: improve `ProjectService` logic

--- src/services/project.service.ts
+++ src/services/project.service.ts
@@ -1,36 +1,40 @@
 import { db } from "@config/db.config";
-import * as UserDAL from "@dal/auth.dal";
+import * as UserDAL from "@dal/user.dal";
 import * as ProjectDAL from "@dal/project.dal";
 import * as ReleaseDAL from "@dal/release.dal";
 import { Project } from "@models/project.model";
 import { ApiError } from "@utils/error";
 import { StatusCodes } from "http-status-codes";
-import {
-  ProjectDTO,
-  ProjectIdQueryParam,
-  ProjectIdQueryParamSchema,
-  ProjectSchema,
-} from "@schemas/project.schema";
-import { UserIdQueryParam, UserIdQueryParamSchema } from "@schemas/user.schema";
 import { injectable } from "tsyringe";
+
+export interface ProjectDTOFields {
+  id: string;
+  projectName: string;
+  boardType: string;
+  totalReleases: number;
+  apiKey: string;
+  status: string;
+}
 
 @injectable()
 export class ProjectService {
-  public async createProject(payload: ProjectDTO): Promise<Project> {
-    const { userId, projectName, boardType } = await ProjectSchema.parseAsync(payload);
-
+  public async createProject(
+    userId: string,
+    projectName: string,
+    boardType: "ESP32" | "ESP8266",
+  ): Promise<Project> {
     const user = await UserDAL.findUserByPublicId(userId);
 
     if (!user) {
       throw new ApiError("User not found!", StatusCodes.NOT_FOUND);
     }
 
-    if (await ProjectDAL.isProjectNameInUse(user.id!, projectName)) {
+    if (await ProjectDAL.isProjectNameInUse(user.getId(), projectName)) {
       throw new ApiError("Project with this name is already in use!", StatusCodes.CONFLICT);
     }
 
     const projectData = await ProjectDAL.createProject({
-      userId: user.id!,
+      userId: user.getId(),
       name: projectName,
       boardType: boardType,
     });
@@ -38,21 +42,36 @@ export class ProjectService {
     return projectData;
   }
 
-  // TODO get all user projects with pagination......
-  public async getAllProjects(queryParam: UserIdQueryParam): Promise<readonly Project[]> {
-    const { userId } = await UserIdQueryParamSchema.parseAsync(queryParam);
+  public async findProjectByPublicId(id: string) {
+    const project = await ProjectDAL.findProjectByPublicId(id);
+    if (!project) throw new ApiError("Project not found!", StatusCodes.NOT_FOUND);
 
+    return project;
+  }
+
+  // TODO get all user projects with pagination......
+  public async getAllProjects(userId: string): Promise<readonly ProjectDTOFields[]> {
     const user = await UserDAL.findUserByPublicId(userId);
 
     if (!user) {
       throw new ApiError("User not found!", StatusCodes.NOT_FOUND);
     }
 
-    return await ProjectDAL.findProjectsByUserId(user.id!);
+    const projects = await ProjectDAL.findProjectsByUserId(user.id!);
+
+    return projects.map((e, i) => {
+      return {
+        id: e.getPublicId(),
+        projectName: e.getProjectName(),
+        boardType: e.getBoardType(),
+        totalReleases: e.Releases?.length,
+        status: e.Releases?.at(0)?.getChannel(),
+        apiKey: e.getApiKey(),
+      };
+    }) as ProjectDTOFields[];
   }
 
-  public async softDeleteProject(queryParam: ProjectIdQueryParam): Promise<void> {
-    const { projectId } = await ProjectIdQueryParamSchema.parseAsync(queryParam);
+  public async softDeleteProject(projectId: string): Promise<void> {
     const project = await ProjectDAL.findProjectByPublicId(projectId);
 
     if (!project) {
@@ -61,8 +80,8 @@ export class ProjectService {
 
     const transaction = await db.transaction();
     try {
-      await ReleaseDAL.deleteAllReleases(project.id!, transaction);
-      await ProjectDAL.deleteProject(project.id!, transaction);
+      await ReleaseDAL.deleteAllReleases(project.getId(), transaction);
+      await ProjectDAL.deleteProject(project.getId(), transaction);
       await transaction.commit();
     } catch (error) {
       await transaction.rollback();
