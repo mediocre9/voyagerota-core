b4808ac9fc6228aa5450a70e15fe6c43d0014335
Mediocre <76650975+mediocre9@users.noreply.github.com>
2026-01-25 19:19:00 +0500

    Revert "refactor: replace `console.log` with `Logger` for better logging"
    This reverts commit 3a5a8a8fc298664b1f57924e0ce2e4d6b1fa773e.

--- src/config/redis.connection.config.ts
+++ src/config/redis.connection.config.ts
@@ -1,12 +1,11 @@
-import { Logger } from "@utils/logger";
 import { Redis } from "ioredis";
 
 export const redis = new Redis({ maxRetriesPerRequest: null });
 
 redis.on("ready", () => {
-  Logger.info("Redis connection up!");
+  console.log("Redis connection up!");
 });
 
 redis.on("error", (error) => {
-  Logger.error("Redis Failed Error: %s", error.message);
+  console.log("Redis Failed Error: ", error);
 });
