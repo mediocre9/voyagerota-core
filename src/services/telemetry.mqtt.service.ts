a6c106c0747006c61d2c2e511fd2dd234a55ae2a
mediocre9 <mirzafahadzia9@gmail.com>
2026-01-25 17:53:44 +0500

    refactor: use environment variables

--- src/services/telemetry.mqtt.service.ts
+++ src/services/telemetry.mqtt.service.ts
@@ -2,6 +2,7 @@ import { Logger } from "@utils/logger";
 import mqtt from "mqtt";
 import { Server } from "socket.io";
 import { log } from "console";
+import { EnvConfig } from "@config/config";
 
 const io = new Server({
   transports: ["websocket"],
@@ -15,12 +16,12 @@ io.on("connection", (_) => {
   Logger.info("webscoket server up!");
 });
 
-const client = mqtt.connect("mqtts://z8112f26.ala.asia-southeast1.emqxsl.com:8883", {
-  port: 8883,
+const client = mqtt.connect(EnvConfig.MQTT_ADDRESS, {
   clean: true,
-  clientId: "voyager",
-  username: "voyager_telemetry_sub_service",
-  password: "123",
+  port: EnvConfig.MQTT_PORT,
+  clientId: EnvConfig.MQTT_CLIENT_ID,
+  username: EnvConfig.MQTT_USERNAME,
+  password: EnvConfig.MQTT_PASSWORD,
   connectTimeout: 4000,
   reconnectPeriod: 1000,
 });
