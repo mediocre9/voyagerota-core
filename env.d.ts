5aa089efef4f97618b88ce8ad3087e6d0333b44f
mediocre9 <mirzafahadzia9@gmail.com>
2026-01-25 17:23:08 +0500

    refactor: add type definitions for env variables

--- env.d.ts
+++ env.d.ts
@@ -1,5 +1,22 @@
 declare namespace NodeJS {
   interface ProcessEnv {
+    PORT: string;
+    SESSION_SECRET: string;
+    BASE_URL: string;
+    ENVIRONMENT: string;
+
+    DB_PORT: string;
+    DB_NAME: string;
+    DB_HOST: string;
+    DB_USERNAME: string;
+    DB_PASSWORD: string;
+
+    MQTT_PORT: string;
+    MQTT_ADDRESS: string;
+    MQTT_CLIENT: string;
+    MQTT_USERNAME: string;
+    MQTT_PASSWORD: string;
+
     BROKER_BASIC_AUTH_CREDENTIALS: string;
   }
 }
