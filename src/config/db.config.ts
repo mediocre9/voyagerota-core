90eb7c495aebff43a990390dffafe61fed399d9f
mediocre9 <mirzafahadzia9@gmail.com>
2026-01-25 17:25:59 +0500

    refactor: use environment variables

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,14 +1,15 @@
 import { Logger } from "@utils/logger";
-import { isDevEnvironment } from "@utils/utils";
+
 import { Sequelize } from "sequelize";
+import { isDevEnvironment, EnvConfig } from "./config";
 
 export const db = new Sequelize({
-  port: 3306,
-  database: "voyager_ota_dev",
-  host: "localhost",
+  port: EnvConfig.DB_PORT,
+  database: EnvConfig.DB_NAME,
+  host: EnvConfig.DB_HOST,
   dialect: "mariadb",
-  username: "root",
-  password: "123",
+  username: EnvConfig.DB_USERNAME,
+  password: EnvConfig.DB_PASSWORD,
   pool: {
     max: 5,
     min: 0,
