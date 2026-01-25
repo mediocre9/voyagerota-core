be0201ede0479b35019d0ba18f064d5731dc36fd
Mediocre <76650975+mediocre9@users.noreply.github.com>
2026-01-25 19:19:00 +0500

    Revert "refactor: improve `logger` service"
    This reverts commit f0dacf44c96551aa225c5026fa286d003be70b4d.

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,65 +1,63 @@
 import winston, { format } from "winston";
 import path from "path";
-import chalk from "chalk";
-import { isDevEnvironment } from "@config/config";
 
 const logger = winston.createLogger({
   transports: [
+    new winston.transports.Console({
+      level: "error",
+      format: format.combine(
+        format.colorize({ all: true }),
+        format.printf((info): string => {
+          if (typeof info.level === "string" && typeof info.message === "string") {
+            return `${info.level}: ${info.message} at ${new Date().toString()}`;
+          }
+          return "";
+        })
+      ),
+    }),
+
+    new winston.transports.Console({
+      level: "debug",
+      format: format.combine(
+        format.colorize({ all: true, colors: { debug: "yellow" } }),
+        format.printf(
+          ({ level, message }) => `${level}: ${message as string} at ${new Date().toString()}`
+        )
+      ),
+    }),
+
+    new winston.transports.Console({
+      level: "info",
+      format: format.combine(
+        format.colorize({ all: true }),
+        format.printf(
+          ({ level, message }) => `${level}: ${message as string} at ${new Date().toString()}`
+        )
+      ),
+    }),
+
     new winston.transports.File({
       filename: path.join("logs", "error.log"),
       level: "error",
       maxsize: 10000,
     }),
-
-    new winston.transports.Console({
-      format: format.combine(
-        format.timestamp({ format: () => new Date().toUTCString() }),
-        format.splat(),
-        format.printf(({ level, message, _, timestamp }) => {
-          if (isDevEnvironment()) {
-            const formattedMessage = `[${chalk.bold.underline(level.toUpperCase())}] : ${chalk(
-              message as string,
-            )} ${chalk.dim(` - ${timestamp as string}`)}`;
-
-            switch (level) {
-              case "info":
-                return chalk.rgb(0, 255, 64).visible(formattedMessage); // purple....
-
-              case "debug":
-                return chalk.rgb(255, 213, 0).visible(formattedMessage); // yellow......
-
-              case "warn":
-                return chalk.rgb(255, 106, 0).visible(formattedMessage); // orange....
-
-              default:
-                return chalk.rgb(255, 0, 0).visible(formattedMessage); // red....
-            }
-          } else {
-            return `[${level.toUpperCase()}] : ${message as string} - ${timestamp as string}`;
-          }
-        }),
-      ),
-    }),
-  ],
-  exceptionHandlers: [
-    new winston.transports.File({ filename: path.join("logs", "exceptions.log") }),
   ],
 });
 
 export const Logger = {
-  info(message: string, ...args: string[]) {
-    logger.info(message, args);
+  info(message: string) {
+    logger.info(message);
   },
 
-  debug(message: string, ...args: string[]) {
-    logger.debug(message, args);
+  debug(message: string) {
+    logger.debug(message);
   },
 
-  warn(message: string, ...args: string[]) {
-    logger.warn(message, args);
+  warn(message: string) {
+    logger.warn(message);
   },
 
-  error(message: string, ...args: string[]) {
-    logger.error(message, args);
+  error(message: string) {
+    logger.error(message);
   },
 };
