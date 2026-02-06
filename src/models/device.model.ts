a826e7d9c001a0026144da42a4e1f8bc115f29b5
mediocre9 <mirzafahadzia9@gmail.com>
2026-02-06 12:32:05 +0500

    feat: add `Device` model

--- src/models/device.model.ts
+++ src/models/device.model.ts
@@ -6,36 +6,81 @@ import {
   InferCreationAttributes,
   Model,
 } from "sequelize";
-import { Project } from "./project.model";
+import { Release } from "./release.model";
+import { nanoid } from "nanoid";
 
-export class DeviceOTAStatus extends Model<
-  InferAttributes<DeviceOTAStatus>,
-  InferCreationAttributes<DeviceOTAStatus>
-> {
+export type DeviceStatus = "success" | "failed" | "unknown";
+export class Device extends Model<InferAttributes<Device>, InferCreationAttributes<Device>> {
   declare id?: number;
-  declare project_id_fk?: number;
+  declare release_id_fk?: number;
+  declare public_id?: string;
+  declare status: DeviceStatus;
   declare mac_address: string;
-  declare deleted_at: CreationOptional<Date>;
+  declare deleted_at?: CreationOptional<Date>;
+  declare updated_at?: CreationOptional<Date>;
+
+  getId() {
+    return this.getDataValue("id")!;
+  }
+
+  getReleaseForeignKeyId() {
+    return this.getDataValue("release_id_fk")!;
+  }
+
+  getPublicId() {
+    return this.getDataValue("public_id")!;
+  }
+
+  getMacAddress() {
+    return this.getDataValue("mac_address");
+  }
+
+  getStatus() {
+    return this.getDataValue("status");
+  }
+
+  toDTO() {
+    return {
+      id: this.getPublicId(),
+      macAddress: this.getMacAddress(),
+      status: this.getStatus(),
+    };
+  }
 }
 
-DeviceOTAStatus.init(
+Device.init(
   {
     id: {
       type: DataTypes.BIGINT.UNSIGNED,
       primaryKey: true,
       autoIncrement: true,
     },
-    project_id_fk: {
+    public_id: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      defaultValue() {
+        return nanoid(21);
+      },
+    },
+    release_id_fk: {
       type: DataTypes.BIGINT.UNSIGNED,
       allowNull: false,
       references: {
-        model: Project,
+        model: Release,
         key: "id",
       },
+    },
+    status: {
+      type: DataTypes.ENUM("success", "failed", "unknown"),
+      defaultValue: "unknown",
     },
     mac_address: {
       type: DataTypes.STRING,
       unique: true,
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      allowNull: true,
     },
     deleted_at: {
       type: DataTypes.DATE,
